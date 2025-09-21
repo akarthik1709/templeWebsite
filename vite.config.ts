47510920e88a8c41baf3da90a7622c1138e30ef1
Karthik <akarthik1709@gmail.com>
2025-09-21 19:09:53 -0400

    Modified css and added changes to the carousel

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig ({
   plugins: [react()],
 })
-module.exports = {
-  build: {
-    outDir: '../build',
-    assetsDir: 'assets',
-    rollupOptions: {
-      external: ['react','react-dom'],
-    },
-  },
-}
