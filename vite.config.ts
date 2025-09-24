6119b61f092892d2f8a2cf46205441413aaad6fe
Karthik <akarthik1709@gmail.com>
2025-09-24 18:36:53 -0400

    Added changes to the generate index and action since the token was failing in the opriginal code

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // string shorthand: http://localhost:5173/create-payment-intent -> http://localhost:4242/create-payment-intent
-      '/create-payment-intent': 'http://localhost:4242/create-payment-intent',
+      '/create-payment-intent': 'http://127.0.0.1:4242',
     },
   },
 });
