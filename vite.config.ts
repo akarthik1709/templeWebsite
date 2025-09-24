a5e88f20af27f5815884518027ea1923f0b0092f
Karthik <akarthik1709@gmail.com>
2025-09-24 17:39:23 -0400

    Modified css and added changes to the carousel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       // string shorthand: http://localhost:5173/create-payment-intent -> http://localhost:4242/create-payment-intent
-      '/create-payment-intent': 'http://localhost:4242',
+      '/create-payment-intent': 'http://localhost:4242/create-payment-intent',
     },
   },
 });
