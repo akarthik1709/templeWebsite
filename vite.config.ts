a7679a10038ec2da08fbda364705f81c5b1a578a
Karthik <akarthik1709@gmail.com>
2025-09-26 15:38:17 -0400

    Added changes to the package.jsona nd Payements

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePluginNode } from 'vite-plugin-node';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { 
-    proxy: {
-      '/api/process-google-pay': 'http://localhost:4242',
-    },
-  },
+  plugins: [
+    react(),
+    ...VitePluginNode({
+      // Node-plugin options
+      adapter: 'express',
+      appPath: './server/server.js',
+      exportName: 'viteNodeApp',
+      tsCompiler: 'esbuild',
+    })
+  ],
+  server: {
+    port: 3000 // Your frontend port
+  }
 });
